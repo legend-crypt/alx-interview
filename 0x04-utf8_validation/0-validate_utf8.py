328d2774057608948e7f199f24565a9898adcc32
lord <konadulordkweku@gmail.com>
2024-11-02 07:11:52 +0000

    feat: Add module that determine if a character set is utf-8 encoded

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+No module was imported
+"""
+
+
+def validUTF8(data):
+    """Deteremines if a data set represents a valid UTF-8 encoding"""
+    num_of_bytes = 0
+    for num in data:
+        if num_of_bytes > 0:
+            if num >> 6 != 0b10:
+                return False
+            num_of_bytes -= 1
+        else:
+            if num >> 7 == 0:
+                num_of_bytes = 0
+            elif num >> 5 == 0b110:
+                num_of_bytes = 1
+            elif num >> 4 == 0b1110:
+                num_of_bytes = 2
+            elif num >> 3 == 0b11110:
+                num_of_bytes = 3
+            else:
+                return False
+    return num_of_bytes == 0
